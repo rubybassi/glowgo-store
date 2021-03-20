edb384825aa03a9ae3ccd1a35f51d1ccad2c61e5
Sandeep Bassi <thecodingfiles@gmail.com>
2021-03-20 23:37:07 +0000

    set up intial code for sign up post route

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const { User } = require('../../database/index'); 
 
-// @POST sign in
-// <Route exact path="/login" component={Login} />
+// @POST register - add validate body middleware, bcrypt password then send jwt token and non-sensitive user info
+router.post('/register', async (req, res) => {
+  try {
+    const { firstname, surname, email, password } = req.body;  
+    const user = await User.create({ firstname, surname, email, password });
+    res.status(200).json(user); 
+  } catch (err) {
+    res
+      .status(404)
+      .json({
+        error: "Your request could not be processed. Please try again.",
+      });
+  }
+});
 
-// @POST register 
-// <Route exact path="/register" component={Register} />
-
-// @POST CART 
-// <Route exact path="/cart" component={Cart} />
-
+// @POST sign in - add validate body middleware, then send jwt token and non-sensitive user info
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;  
+    //const user = await User.find({ email: email });
+    res.status(200).json(user); 
+  } catch (err) {
+    res
+      .status(404)
+      .json({
+        error: "Your request could not be processed. Please try again.",
+      });
+  }
+});
 
 module.exports = router;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 // protected routes that will require auth to enter
-//<Route exact path="/user/order" component={Order} />
+
+// @post cart
 //<Route exact path="/user/checkout" component={Checkout} />
 
+// @get order history
+//<Route exact path="/user/order" component={Order} />
 
 
 module.exports = router;
