221fb371e97440931512e8414626d0e204370666
Sandeep Bassi <thecodingfiles@gmail.com>
2021-03-25 02:24:49 +0000

    added order model

--- database/models/Order.js
+++ database/models/Order.js
@@ -1,22 +1,47 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const OrderSchema = new Schema({
-  name: {
-    type: String,
+const OrderSchema = new Schema(
+  {
+    userID: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    payment: {
+      name: { type: String },
+      number: { type: Number },
+      expiry: { type: Number },
+      cvv: { type: Number },
+    },
+    shipping: {
+      firstname: { type: String },
+      surname: { type: String },
+      address1: { type: String },
+      address2: { type: String },
+      city: { type: String },
+      postcode: { type: String },
+    },
+    orderTotal: {
+      type: Number,
+    },
+    shippingFee: {
+      type: String,
+    },
+    cartItems: [
+      {
+        productID: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        price: { type: Number },
+        qty: { type: Number },
+      },
+    ],
   },
-  description: {
-    type: String,
-  },
-  logo: {
-    type: String,
-  },
-  products: [{
-    type: Schema.Types.ObjectId,
-    ref: "Product",
-    default: null,
-  }],
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Order = mongoose.model("Order", OrderSchema);
 
