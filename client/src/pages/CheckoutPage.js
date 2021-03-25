ead7c3cd9356518aef04bcf23053c5d7670f7e03
Sandeep Bassi <thecodingfiles@gmail.com>
2021-03-25 18:07:31 +0000

    renamed checkout folder for build error

--- client/src/pages/CheckoutPage.js
+++ client/src/pages/CheckoutPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
-import Checkout from "../components/Checkout/Checkout";
+import Checkout from "../components/CheckoutProcess/Checkout";
 
 const CheckoutPage = () => {
   return (
